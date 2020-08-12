85ba91d433b719bda19e8ff44948545aacee5849
aaronkotch <aaronssikua@gmail.com>
2020-08-13 00:31:04 +0800

    working text input controller in listview.builder

--- lib/newTask_view.dart
+++ lib/newTask_view.dart
@@ -8,140 +8,120 @@ class NewTask extends StatefulWidget {
 }
 
 class _NewTaskState extends State<NewTask> {
-
   TextEditingController titleInputController;
-  TextEditingController tasksInputController = TextEditingController();
-
+  TextEditingController tasksInputController = new TextEditingController();
   String saved = '';
+
+  List<TextEditingController> _controller = new List();
+
+  List<String> lItems = [];
+
+  List<Widget> list = new List();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          body: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: CustomScrollView(
-                slivers: <Widget>[
-                  NewTaskAppBar(),
-                  SliverList(
-                    delegate: SliverChildListDelegate([
-                      Column(
-                        children: <Widget>[
-                          Material(
-                            color: Colors.transparent,
-                            child: Container(
-                              padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                              child: TextField(
-                                textAlign: TextAlign.left,
-                                cursorColor: Colors.blueAccent,
-                                controller: tasksInputController,
-                                style: TextStyle(
-                                  fontFamily: "SourceSansPro",
-                                  fontWeight: FontWeight.w300,
-                                ),
-                                decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor: Colors.grey[200],
-                                  contentPadding: EdgeInsets.only(
-                                      left: 20, top: 20, bottom: 20),
-                                  border: OutlineInputBorder(
-                                      borderSide: BorderSide()),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                      color: Colors.grey[200],
-                                    ),
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.grey[200],
-                                      ),
-                                      borderRadius: BorderRadius.circular(8)),
-                                  hintText: 'Title',
-                                  hintStyle: TextStyle(
-                                    fontFamily: "SourceSansPro",
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                ),
-                              ),
-                            ),
+      child: new Scaffold(
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              child: new Material(
+                color: Colors.transparent,
+                child: Container(
+                    margin: EdgeInsets.all(16),
+                    child: TextField(
+                      cursorColor: Colors.blueAccent,
+                      controller: tasksInputController,
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontWeight: FontWeight.w300,
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        contentPadding:
+                            EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                        border: OutlineInputBorder(borderSide: BorderSide()),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[200],
                           ),
-                          Material(
-                            color: Colors.transparent,
-                            child: Container(
-                              padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                              child: TextField(
-                                textAlign: TextAlign.left,
-                                cursorColor: Colors.blueAccent,
-                                controller: tasksInputController,
-                                style: TextStyle(
-                                  fontFamily: "SourceSansPro",
-                                  fontWeight: FontWeight.w300,
-                                ),
-                                decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor: Colors.grey[200],
-                                  contentPadding: EdgeInsets.only(
-                                      left: 20, top: 20, bottom: 20),
-                                  border: OutlineInputBorder(
-                                      borderSide: BorderSide()),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                      color: Colors.grey[200],
-                                    ),
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.grey[200],
-                                      ),
-                                      borderRadius: BorderRadius.circular(8)),
-                                  hintText: 'Title',
-                                  hintStyle: TextStyle(
-                                    fontFamily: "SourceSansPro",
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                ),
-                              ),
-                            ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[200],
                           ),
-                          Container(
-                            height: 200,
-                            child: ButtonTheme(
-                              child: FlatButton(
-                                child: Text("PRINT"),
-                                onPressed: () {
-                                  setState(() {
-                                    saved = tasksInputController.text;
-                                  });
-
-                                  printList();
-                                },
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ]),
-                  )
-                ],
-              ))),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        hintText: 'Task',
+                        hintStyle: TextStyle(
+                          fontFamily: "SourceSansPro",
+                          fontWeight: FontWeight.w300,
+                        ),
+                        focusColor: Colors.yellow[400],
+                      ),
+                      onSubmitted: (text) {
+                        lItems.add(text);
+                        tasksInputController.clear();
+                        setState(() {});
+                      },
+                    )),
+              ),
+            ),
+            Container(
+              child: FlatButton(
+                child: Text("PRINT"),
+                onPressed: () {
+                  _controller.add(new TextEditingController());
+                  int i = _controller.length - 1;
+                  list.add(new TextField(
+                    controller: _controller[i],
+                    decoration: InputDecoration(
+                        hintText: 'Hint ${_controller.length+1}'),));
+                  print(_controller);
+                  setState(() {});
+                },
+              ),
+            ),
+            Container(
+              child: new Expanded(
+                child: new ListView.builder(
+                    itemCount: _controller.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      Widget widget = list.elementAt(index);
+                      return widget;
+                    }),
+              ),
+            ),
+            Container(
+                child: new Expanded(
+                    child: new ListView.builder(
+                        itemCount: lItems.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return new Text(lItems[index]);
+                        }))),
+          ],
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: () {
+            int i = _controller.length - 1;
+            print(_controller[1].text);
+          },
+          child: new Icon(Icons.add),
+        ),
+      ),
     );
   }
 
   void printList() {
-
-    var testList = [
-      saved,
-      "bruh"
-    ];
+    var testList = [saved, "bruh"];
 
     testList.add("nice");
 
     print(testList);
   }
-
 }
 
 class NewTaskAppBar extends StatefulWidget {
