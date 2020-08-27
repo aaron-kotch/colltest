f56a651c33de2169520cf19e2e846262d9725068
aaronkotch <aaronssikua@gmail.com>
2020-08-27 22:07:01 +0800

    Worked on section cards.

--- lib/newTask_view.dart
+++ lib/newTask_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewTask extends StatefulWidget {
@@ -14,7 +15,6 @@ class _NewTaskState extends State<NewTask> {
   TextEditingController tasksInputController = new TextEditingController();
 
   List<TextEditingController> _controller = new List();
-  List<TextEditingController> _childController = new List();
 
   List<TextEditingController> _childTextController = new List();
 
@@ -34,91 +34,28 @@ class _NewTaskState extends State<NewTask> {
 
   final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
 
-  List keyList = [GlobalKey<AnimatedListState>()];
+  List<GlobalKey<AnimatedListState>> cardKey = [new GlobalKey<AnimatedListState>()];
 
   bool clicked = false;
 
-  List<int> _items = [];
-
-  int counter = 0;
+  int cardCount = 0;
 
   int pos;
+  int pos1;
 
-  int index1 = -1;
+  int childTextControllerIndex = -1;
+
   int keyIndex = -1;
 
   double cardHeight = 250;
 
-  Widget childCard(BuildContext context, int index, animation) {
-    return SizeTransition(
-        sizeFactor:
-            CurvedAnimation(parent: animation, curve: Curves.easeInOutCubic),
-        child: Container(
-            padding: EdgeInsets.only(left: 24, right: 8, top: 8, bottom: 4),
-            child: Container(
-                child: Material(
-                  shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
-                  elevation: 0,
-                  child: Column(
-                    children: <Widget>[
-                      new TextField(
-                        controller: testList[index1][index],
-                        autofocus: false,
-                        maxLines: null,
-                        keyboardType: TextInputType.multiline,
-                        style: TextStyle(
-                          fontFamily: "SourceSansPro",
-                          fontWeight: FontWeight.w800,
-                          color: Colors.grey[800],
-                          fontSize: 15,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: 'child',
-                          hintStyle: TextStyle(
-                            fontFamily: "SourceSansPro",
-                            fontWeight: FontWeight.w400,
-                            color: Colors.grey[500],
-                            fontSize: 15,
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey[100],
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.grey[100],
-                              )),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(0),
-                                bottomRight: Radius.circular(0),
-                                topLeft: Radius.circular(8),
-                                topRight: Radius.circular(8),
-                              ),
-                              borderSide: BorderSide(
-                                color: Colors.grey[100],
-                              )),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.blueAccent,
-                            ),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(0),
-                              bottomRight: Radius.circular(0),
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8),
-                            ),
-                          ),
-                          contentPadding: EdgeInsets.only(
-                              left: 16, right: 16, top: 12, bottom: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-            ))));
-  }
+  List<Widget> cList = new List();
+
+  List<Widget> newList = [];
+
+  TextEditingController controller = TextEditingController();
 
   Widget sectionCard(BuildContext context, int index, animation) {
-
     return SizeTransition(
       sizeFactor: CurvedAnimation(
         parent: animation,
@@ -187,52 +124,88 @@ class _NewTaskState extends State<NewTask> {
                           ),
                         ),
                       ),
-                      AnimatedList(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          scrollDirection: Axis.vertical,
-                          key: keyList[keyIndex],
-                          initialItemCount: _childController.length,
-                          itemBuilder:
-                              (BuildContext context, int index, animation) {
-
-                            return childCard(context, index, animation);
-                          }),
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding:
-                                EdgeInsets.only(left: 24, top: 4, bottom: 4),
-                            child: ButtonTheme(
-                              minWidth: 10,
-                              height: 30,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: FlatButton.icon(
-                                icon: Icon(Icons.add,
-                                    color: Colors.white, size: 12),
-                                disabledColor: Colors.blueAccent,
-                                color: Colors.blueAccent,
-                                splashColor: Colors.deepOrange[100],
-                                label: Text("ADD",
-                                    style: TextStyle(
-                                        fontFamily: "SourceSansPro",
-                                        fontWeight: FontWeight.w800,
-                                        fontSize: 12,
-                                        color: Colors.white,
-                                        letterSpacing: 1.25)),
-                                onPressed: () {
-
-                                  testList[index1].add(new TextEditingController());
-
-                                  keyList[keyIndex].currentState.insertItem(0,
-                                      duration: const Duration(milliseconds: 500));
-                                  setState(() {});
-                                },
-                              ),
+                      Container(
+                        padding: EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 4),
+                        child: TextField(
+                          controller: _childTextController[index],
+                          maxLines: null,
+                          keyboardType: TextInputType.multiline,
+                          style: TextStyle(
+                            fontFamily: "SourceSansPro",
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey[800],
+                            fontSize: 15,
+                          ),
+                          decoration: InputDecoration(
+                            labelText:'Description',
+                            labelStyle: TextStyle(
+                              fontFamily: "SourceSansPro",
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                              height: 1,
                             ),
-                          ))
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey[100],
+                                )),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(4),
+                                borderSide: BorderSide(
+                                  color: Colors.grey[500],
+                                )),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(4),
+                                borderSide: BorderSide(
+                                  color: Colors.blueAccent,
+                                )
+                            ),
+                            contentPadding: EdgeInsets.only(
+                                left: 16, right: 16, top: 16, bottom: 16),
+                          ),
+                        ),
+                      ),
+                      Container(
+                          padding: EdgeInsets.only(left: 16, bottom: 12, top: 16, right: 16),
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            children: <Widget>[
+                              ButtonTheme(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(4)
+                                ),
+                                disabledColor: Colors.orange[500],
+                                buttonColor: Colors.orange[500],
+                                child: FlatButton(
+                                  disabledColor: Colors.pink[500],
+                                  child: Text("ASSIGN MEMBERS",
+                                    style: TextStyle(
+                                      fontFamily: "SourceSansPro",
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.white,
+                                      letterSpacing: 1.25,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(left: 16),
+                                child: Icon(Icons.check_circle, size: 20, color: Colors.greenAccent[700]),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(left: 8),
+                                child: Text("Assigned to Guano + 2",
+                                  style: TextStyle(
+                                    fontFamily: "SourceSansPro",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15,
+                                    color: Colors.blueAccent[100],
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                      ),
                     ],
                   )))),
     );
@@ -255,6 +228,7 @@ class _NewTaskState extends State<NewTask> {
               Container(
                 color: Colors.blue[200],
                 height: scrHeight / 3,
+                margin: EdgeInsets.only(bottom: 8),
               ),
               AnimatedList(
                   shrinkWrap: true,
@@ -288,13 +262,11 @@ class _NewTaskState extends State<NewTask> {
                                   color: Colors.white,
                                   letterSpacing: 1.25)),
                           onPressed: () {
-                            keyIndex++;
 
-                            keyList.add(new GlobalKey<AnimatedListState>());
+                            childTextControllerIndex++;
+                            _childTextController.add(new TextEditingController());
 
                             testList.add(_childTextController);
-
-                            index1++;
 
                             if (_controller.length == 0) {
                               _controller.add(new TextEditingController());
@@ -313,28 +285,7 @@ class _NewTaskState extends State<NewTask> {
                           },
                         ),
                       ),
-                      ButtonTheme(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: FlatButton.icon(
-                          color: Colors.pink[500],
-                          splashColor: Colors.deepPurple[100],
-                          icon: Icon(Icons.add, color: Colors.white),
-                          label: Text("Add into inner list",
-                              style: TextStyle(
-                                  fontFamily: "SourceSansPro",
-                                  fontWeight: FontWeight.w800,
-                                  color: Colors.white,
-                                  letterSpacing: 1.25)),
-                          onPressed: () {
 
-                            _childTextController.add(new TextEditingController());
-
-                             testList.add(_childTextController);
-                          },
-                        ),
-                      ),
                       ButtonTheme(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
@@ -350,10 +301,10 @@ class _NewTaskState extends State<NewTask> {
                                   color: Colors.white,
                                   letterSpacing: 1.25)),
                           onPressed: () {
-                            print(testList);
-                            print(_childTextController.length);
-                            print(testList.sublist(1));
-                            print(testList[0]);
+
+                            print(_childTextController);
+
+                            //print(testList.sublist(1));
                           },
                         ),
                       )
@@ -376,8 +327,6 @@ class _NewTaskState extends State<NewTask> {
               print("button disabled!");
             }
 
-            String len = _controller.length.toString();
-
             print(_controller[pos].text);
           },
           child: new Icon(Icons.add),
@@ -398,6 +347,7 @@ class _NewTaskState extends State<NewTask> {
       },
     );
   }
+
 }
 
 class NewTaskAppBar extends StatefulWidget {
